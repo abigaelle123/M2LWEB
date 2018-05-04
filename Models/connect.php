c400ca375e97171fc72691c9b0df092b08d2acfa
Chloesbrs <chloe.sbrs@orange.fr>
2018-05-04 16:34:28 +0200

    maj connect.php

--- Models/connect.php
+++ Models/connect.php
@@ -1,7 +1,7 @@
 <?php
     try
     {
-       $bdd = new PDO("mysql:host=127.0.0.1;dbname=m2l;charset=utf8","root",""); // connection bdd
+       $bdd = new PDO("mysql:host=127.0.0.1;dbname=m2l;charset=utf8","root","root"); // connection bdd
     }
     catch(Exception $e)
     {

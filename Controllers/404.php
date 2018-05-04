d7ced5fa8953b052e369d3aec77dbfc0260ed2d5
abigaelle123 <abigaellehouri@gmail.com>
2018-05-04 16:23:38 +0200

    resetall

--- Controllers/404.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-require "Views/404.php";
-
-?>
--- Controllers/disconnect.php
+++
@@ -1,8 +0,0 @@
-<?php
-    session_start();
-    unset($_SESSION);
-    session_destroy();
-    setcookie('auth','',time()-3600,'/','localhost');
-    unset($_COOKIE['auth']);
-    header('Location:'.BASE_URL);
-?>

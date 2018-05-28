92376db93cc1ddd16a29226f86d1629bf537cdba
MarkoStefanovic-98 <markostef@hotmail.fr>
2018-05-28 14:51:55 +0200

    MVC pour envois msg fait

--- Controllers/messagerie.php
+++ Controllers/messagerie.php
@@ -8,32 +8,15 @@ if(isset($_SESSION))
     
  if(isset($_POST['submit']))
      
-    {
-        
-        $requete = $bdd->prepare("SELECT id_s FROM salarie WHERE mail = :mail");
-        $requete->bindValue(':mail',$_POST['destinataire'],PDO::PARAM_STR);
-        $requete->execute();
-        if($reponse = $requete->fetch())
         {
-            $requete = $bdd->prepare("INSERT INTO messages(titre,contenu,id_exp,id_dest)
-            VALUES(:titre,:contenu,:exp,:dest)");
-            $requete->bindValue(':titre',$_POST['titre'],PDO::PARAM_STR);
-            $requete->bindValue(':contenu',$_POST['contenu'],PDO::PARAM_STR);
-            $requete->bindValue(':exp',$id_s,PDO::PARAM_INT);
-            $requete->bindValue(':dest',$reponse['id_s'],PDO::PARAM_INT);
-            $requete->execute();
-            echo"<center><h4>message envoyé</h4></center>";
+        sendMessage($id_s); 
+        echo"<center><h4>Message envoyé !</h4></center>";
         }
         else
         {
-            echo"<center><h4>Destinataire inconnu</h4></center>";
+            echo"<center><h4>Destinataire inconnu !</h4></center>";
         }
          
-    }
-        else
-        {
-            echo"<center><h4>Destinataire inconnu</h4></center>";
-        }
 require 'Views/messagerie.php';
 }
 else
--- Models/messagerie.php
+++ Models/messagerie.php
@@ -2,19 +2,18 @@
 
 function sendMessage($id_s){
     global $bdd;
-                $requete = $bdd->prepare("INSERT INTO messages(titre,contenu,id_exp,id_dest)
+        $requete = $bdd->prepare("SELECT id_s FROM salarie WHERE mail = :mail");
+        $requete->bindValue(':mail',$_POST['destinataire'],PDO::PARAM_STR);
+        $requete->execute();
+        if($reponse = $requete->fetch())
+        {
+            $requete = $bdd->prepare("INSERT INTO messages(titre,contenu,id_exp,id_dest)
             VALUES(:titre,:contenu,:exp,:dest)");
             $requete->bindValue(':titre',$_POST['titre'],PDO::PARAM_STR);
             $requete->bindValue(':contenu',$_POST['contenu'],PDO::PARAM_STR);
             $requete->bindValue(':exp',$id_s,PDO::PARAM_INT);
             $requete->bindValue(':dest',$reponse['id_s'],PDO::PARAM_INT);
             $requete->execute();
-}
-
-function selectDestinataire($id_s){
-    global $bdd;
-        $requete = $bdd->prepare("SELECT id_s FROM salarie WHERE mail = :mail");
-        $requete->bindValue(':mail',$_POST['destinataire'],PDO::PARAM_STR);
-        $requete->execute();
+        }
     
 }

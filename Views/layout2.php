0d364792301d5e4058ac9738256409c8ae9655af
Chloesbrs <chloe.sbrs@orange.fr>
2018-05-31 09:50:42 +0200

    layout
    commit

--- Views/layout2.php
+++ Views/layout2.php
@@ -8,23 +8,23 @@
   <!-- Tell the browser to be responsive to screen width -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="<?= BASE_URL; ?>/Views/plugins/font-awesome/css/font-awesome.min.css">
+  <link rel="stylesheet" href="m2lweb.chloesembres.fr/Views/plugins/font-awesome/css/font-awesome.min.css">
   <!-- Ionicons -->
   <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
   <!-- Theme style -->
-  <link rel="stylesheet" href="<?= BASE_URL; ?>/Views/dist/css/adminlte.min.css">
+  <link rel="stylesheet" href="m2lweb.chloesembres.fr/Views/dist/css/adminlte.min.css">
   <!-- iCheck -->
-  <link rel="stylesheet" href="<?= BASE_URL; ?>/Views/plugins/iCheck/flat/blue.css">
+  <link rel="stylesheet" href="m2lweb.chloesembres.fr/Views/plugins/iCheck/flat/blue.css">
   <!-- Morris chart -->
-  <link rel="stylesheet" href="<?= BASE_URL; ?>/Views/plugins/morris/morris.css">
+  <link rel="stylesheet" href="m2lweb.chloesembres.fr/Views/plugins/morris/morris.css">
   <!-- jvectormap -->
-  <link rel="stylesheet" href="<?= BASE_URL; ?>/Views/plugins/jvectormap/jquery-jvectormap-1.2.2.css">
+  <link rel="stylesheet" href="m2lweb.chloesembres.fr/Views/plugins/jvectormap/jquery-jvectormap-1.2.2.css">
   <!-- Date Picker -->
-  <link rel="stylesheet" href="<?= BASE_URL; ?>/Views/plugins/datepicker/datepicker3.css">
+  <link rel="stylesheet" href="m2lweb.chloesembres.fr/Views/plugins/datepicker/datepicker3.css">
   <!-- Daterange picker -->
-  <link rel="stylesheet" href="<?= BASE_URL; ?>/Views/plugins/daterangepicker/daterangepicker-bs3.css">
+  <link rel="stylesheet" href="m2lweb.chloesembres.fr/Views/plugins/daterangepicker/daterangepicker-bs3.css">
   <!-- bootstrap wysihtml5 - text editor -->
-  <link rel="stylesheet" href="<?= BASE_URL; ?>/Views/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css">
+  <link rel="stylesheet" href="m2lweb.chloesembres.fr/Views/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css">
   <!-- Google Font: Source Sans Pro -->
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
   	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
